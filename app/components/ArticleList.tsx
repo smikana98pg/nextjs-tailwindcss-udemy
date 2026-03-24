d2af98879e5397c83d58c7b953d6c7fe7d52bdc9
Kaname Sudo <s.mikana98.pg@gmail.com>
2026-03-25 00:03:57 +0900

    SSRで取得追加

--- app/components/ArticleList.tsx
+++ app/components/ArticleList.tsx
@@ -1,72 +1,53 @@
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
+import { Article } from "../types";
 
-export const ArticleList = () => {
+type ArticleListProps = {
+  articles: Article[];
+};
+
+export const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
-      <article className="shadow my-4">
-        <Link href="#" className="hover:opacity-75">
-          <Image
-            src="https://picsum.photos/800"
-            alt=""
-            width={1280}
-            height={300}
-          />
-        </Link>
-        <div className="bg-white flex flex-col justify-start p-6">
-          <Link href="#" className="text-blue-700 pb-4 font-bold">
-            Technology
+      {articles.map((article) => (
+        <article className="shadow my-4" key={article.id}>
+          <Link href={`articles/${article.id}`} className="hover:opacity-75">
+            <Image
+              src="https://picsum.photos/800"
+              alt=""
+              width={1280}
+              height={300}
+            />
           </Link>
-          <Link
-            href="#"
-            className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
-          >
-            Next.jsの勉強中
-          </Link>
-          <p className="text-sm pb-3 text-slate-900">Published on 2026/03/23</p>
-          <Link href="#" className="text-slate-900 pb-6">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-            quibusdam, optio corporis aliquam quae iste accusantium tenetur cum
-            molestias possimus consectetur explicabo natus obcaecati doloribus
-            quis aperiam numquam minus neque?
-          </Link>
-          <Link href="#" className="text-pink-800 hover:text-black">
-            続きを読む
-          </Link>
-        </div>
-      </article>
-      <article className="shadow my-4">
-        <Link href="#" className="hover:opacity-75">
-          <Image
-            src="https://picsum.photos/800"
-            alt=""
-            width={1280}
-            height={300}
-          />
-        </Link>
-        <div className="bg-white flex flex-col justify-start p-6">
-          <Link href="#" className="text-blue-700 pb-4 font-bold">
-            Technology
-          </Link>
-          <Link
-            href="#"
-            className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
-          >
-            Next.jsの勉強中
-          </Link>
-          <p className="text-sm pb-3 text-slate-900">Published on 2026/03/23</p>
-          <Link href="#" className="text-slate-900 pb-6">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-            quibusdam, optio corporis aliquam quae iste accusantium tenetur cum
-            molestias possimus consectetur explicabo natus obcaecati doloribus
-            quis aperiam numquam minus neque?
-          </Link>
-          <Link href="#" className="text-pink-800 hover:text-black">
-            続きを読む
-          </Link>
-        </div>
-      </article>
+          <div className="bg-white flex flex-col justify-start p-6">
+            <Link href="#" className="text-blue-700 pb-4 font-bold">
+              Technology
+            </Link>
+            <Link
+              href={`articles/${article.id}`}
+              className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
+            >
+              {article.title}
+            </Link>
+            <p className="text-sm pb-3 text-slate-900">
+              Published on {article.createdAt}
+            </p>
+            <Link
+              href={`articles/${article.id}`}
+              className="text-slate-900 pb-6"
+            >
+              {article.content}
+            </Link>
+            <Link
+              href={`articles/${article.id}`}
+              className="text-pink-800 hover:text-black"
+            >
+              続きを読む
+            </Link>
+          </div>
+        </article>
+      ))}
     </div>
   );
 };
